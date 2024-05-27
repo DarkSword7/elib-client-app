231a895aaee92c6bf2cea74bf37268f8af903650
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-05-27 18:23:00 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-t p-5 text-center">
-      <span>© 2024 Coder&#39;s Book All rights reserved.</span>
+      <span>© 2024 Elibrary All rights reserved.</span>
     </footer>
   );
 };
